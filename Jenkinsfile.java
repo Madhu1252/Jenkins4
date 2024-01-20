059acc044a11756f153ed2a836b1bde4c4fa9f10
Madhu1252 <157124598+Madhu1252@users.noreply.github.com>
2024-01-20 16:49:03 +0000

    Three java files added

--- Jenkinsfile.java
+++ Jenkinsfile.java
@@ -1,5 +1,27 @@
-class Jenkinsfile{
-  public static void main(String args[]){
-    system.out.println("Jenkins");
+pipeline{
+  agent any
+    stages{
+    stage('Hello World'){
+      steps{
+        script{
+        echo 'Running Hello world'
+          sh 'javac HelloWorld.java && java HelloWorld'
+          }
+    }
   }
+stage('Hello Wipro'){
+  steps{
+    script{
+    echo 'Running HEllo wipro'
+      sh 'javac HelloWipro.java && java HelloWipro'
+      }
+}
+}
+stage('Hello Jenkins'){
+  steps{
+    script{
+    echo 'Running Hello Jenkins'
+      sh 'javac HelloJenkins.java && java HelloJenkins'
+      }
+}
 }
